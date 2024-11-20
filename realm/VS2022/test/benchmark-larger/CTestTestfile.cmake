a01b099a437aa72d8fd51845b471a23536a3ed58
gojimmypi <jim@wolfssl.com>
2024-11-20 14:52:45 -0800

    remove cmake extension files that are generated by VS

--- realm/VS2022/test/benchmark-larger/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/workspace/osp-gojimmypi/realm/realm-core-gojimmypi/test/benchmark-larger
-# Build directory: C:/workspace/osp-gojimmypi/realm/VS2022/test/benchmark-larger
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- realm/VS2022/test/object-store/benchmarks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/workspace/osp-gojimmypi/realm/realm-core-gojimmypi/test/object-store/benchmarks
-# Build directory: C:/workspace/osp-gojimmypi/realm/VS2022/test/object-store/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
