c1974822a43d17e2e7ca30bee56651b961686092
gojimmypi <jim@wolfssl.com>
2024-11-20 17:30:25 -0800

    improved brute-force clean

--- realm/clean_realm.bat
+++ realm/clean_realm.bat
@@ -1,4 +1,34 @@
 :: todo add feature to detect if install was user-specific (i.e. used -u param)
+@echo off
 
-del realm-core-gojimmypi\CMakeCache.txt
-rd /s /q "out"
+SET REALM_DIR="realm-core-gojimmypi"
+
+echo "Delete %REALM_DIR%\CMakeCache.txt"
+del         "%REALM_DIR%\CMakeCache.txt"
+
+echo "Delete VS2022\CMakeCache.txt"
+del          VS2022\CMakeCache.txt"
+
+echo "Delete VS2022\*.cmake"
+del         "VS2022\*.cmake"
+
+call :DEL_TREE "%REALM_DIR%\out"
+call :DEL_TREE "VS2022\x64"
+
+goto :DONE
+
+:: Function DEL_TREE [directory name]
+:DEL_TREE
+	echo Calling DEL_TREE: %~1
+	if exist "%~1\" (
+		echo Deleting directory: "%~1"
+		rd /s "%~1"
+	) else (
+		echo Could Not Find "%~1"
+	)
+:: Return to the caller
+goto :EOF
+
+
+:DONE
+echo Done!
