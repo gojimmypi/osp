1d163277e674bc568d619c740975b8a07923e1e1
gojimmypi <jim@wolfssl.com>
2024-12-26 16:17:30 -0800

    cmake_policy SET CMP0009 NEW

--- realm/VS2022/CMakeFiles/VerifyGlobs.cmake
+++ realm/VS2022/CMakeFiles/VerifyGlobs.cmake
@@ -1,2 +1,6 @@
 # CMAKE generated file: DO NOT EDIT!
 # This is an initial placeholder for builds that make look for this file.
+#
+# Typically auto-generated is this CMP0009 to ensure libraries with
+# full paths are treated as ssytem libraries:
+cmake_policy(SET CMP0009 NEW)
