bf1944d2ed0e4b908e1468d9f3ada9f7f8389b96
gojimmypi <jim@wolfssl.com>
2024-12-26 19:13:43 -0800

    create VerifyGlobs.cmake as needed

--- realm/VS2022/CMakeFiles/VerifyGlobs.cmake
+++
@@ -1,6 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# This is an initial placeholder for builds that make look for this file.
-#
-# Typically auto-generated is this CMP0009 to ensure libraries with
-# full paths are treated as ssytem libraries:
-cmake_policy(SET CMP0009 NEW)
